df16d2666dc5256124cc066d09a354b00bb2657c
Julian Wittek <wittek@hm.edu>
2019-04-25 16:52:45 +0200

    initial commit

---
+++ ComputationalGeometryAufgabe2/ComputationalGeometryAufgabe2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ComputationalGeometryAufgabe2
+//
+//  Created by Julian Wittek on 25.04.19.
+//  Copyright © 2019 Julian Wittek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
